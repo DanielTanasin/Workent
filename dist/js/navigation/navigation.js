f436828c1dbf085bac0e3a26b951c8ebe723aef3
Daniel Tanasin <daniel.tanasin@vega.local>
2022-11-21 16:57:21 +0100

    first changes

--- dist/js/navigation/navigation.js
+++ dist/js/navigation/navigation.js
@@ -1,1 +1,49 @@
-console.log('test');
+const link = document.querySelector('.js-nav-item');
+const linkText = document.querySelector('.js-nav-link');
+const icon = document.querySelector('.js-icon');
+const dropMenu =document.querySelector('.js-drop-menu');
+const header = document.querySelector('.js-header');
+const ham = document.querySelector('.js-ham');
+const navMenu = document.querySelector('.js-nav')
+const panel = document.querySelector('.js-drop-accordion')
+
+
+
+
+const openDrop = () => {
+    linkText.classList.add('nav-link--about-active');
+    icon.classList.add('header__icon--active');
+    dropMenu.classList.add('drop-menu--active');
+    link.classList.add('nav-item--about--active')
+}
+
+const closeDrop = () => {
+    linkText.classList.remove('nav-link--about-active');
+    icon.classList.remove('header__icon--active');
+    dropMenu.classList.remove('drop-menu--active');
+    link.classList.remove('nav-item--about--active')
+}
+
+
+const openHam = () => {
+    header.classList.toggle('header--active');
+    ham.classList.toggle('active');
+    navMenu.classList.toggle('header__nav--active');
+    
+}
+
+const openDropAcc = () => {
+    if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+    }else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+}
+
+
+link.addEventListener('click', openDropAcc);
+ham.addEventListener('click', openHam);
+link.addEventListener('mouseover', openDrop);
+link.addEventListener('mouseleave', (e) =>{
+    closeDrop();
+})
